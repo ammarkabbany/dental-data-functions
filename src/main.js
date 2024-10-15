faedcab47e53fa572bc6ae2bb3efbb1fb427aa53
ammarkabbany <ammarss750@gmail.com>
2024-10-15 23:15:20 +0300

    final adjustments

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { Client, Databases, Query, Users } from 'node-appwrite';
+import { Client, Databases, Query, Teams, Users } from 'node-appwrite';
 
 // This Appwrite function will be executed every time your function is triggered
 export default async ({ req, res, log, error }) => {
@@ -10,41 +10,19 @@ export default async ({ req, res, log, error }) => {
     .setKey(req.headers['x-appwrite-key'] ?? '');
   const users = new Users(client);
   const databases = new Databases(client)
-
-  const auth_token = req.headers["authorization"];
-  const envAuthToken = process.env.APPWRITE_FUNCTION_AUTH_TOKEN
-
-  try {
-    const response = await users.list();
-    // Log messages and errors to the Appwrite Console
-    // These logs won't be seen by your end users
-    log(`Total users: ${response.total}`);
-    log(envAuthToken)
-    log(auth_token)
-  } catch(err) {
-    error("Could not list users: " + err.message);
-  }
-
-  // The req object contains the request data
-  if (req.path === "/ping") {
-    // Use res object to respond with text(), json(), or binary()
-    // Don't forget to return a response!
-    return res.text("Pong");
-  }
+  const teams = new Teams(client);
 
   if (req.path === "/total") {
-    if (auth_token === envAuthToken) {
-      const response = await databases.listDocuments("mega_dental_data", "66dda08500057cc4e21c", [Query.limit(1)]);
-      return res.json({ cases: response.total });
-    } else {
-      return res.json({ error: "Invalid auth token", auth_token, envAuthToken });
+    try {
+      const cases = await databases.listDocuments("mega_dental_data", "66dda08500057cc4e21c", [Query.limit(1), Query.select(["date"])]);
+    const doctors = await databases.listDocuments("mega_dental_data", "66dc203400027b5e3c73", [Query.limit(1), Query.select(["$id"])]);
+    const labs = (await teams.list()).total
+    return res.status(200).json({ cases: cases.total, doctors: doctors.total, teams: labs });
+    } catch (e) {
+      log(e);
+      return res.status(404).end()
     }
   }
 
-  return res.json({
-    motto: "Build like a team of hundreds_",
-    learn: "https://appwrite.io/docs",
-    connect: "https://appwrite.io/discord",
-    getInspired: "https://builtwith.appwrite.io",
-  });
+  return res.text("")
 };
