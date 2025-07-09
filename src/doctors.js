d87400aec399956d9ecc30a611d13352e2c18e59
عمار هيثم عبدالله قباني <39908704+ammarkabbany@users.noreply.github.com>
2025-07-09 18:47:17 +0300

    tesatasr

--- src/doctors.js
+++ src/doctors.js
@@ -72,7 +72,8 @@ export default async ({ req, res, log, error }) => {
     const case_ = JSON.parse(req.body);
     // TODO: update the doctor's due
     const oldCase = await databases.getDocument(DB_ID, COLLECTION_CASES, case_.$id);
-    log(JSON.stringify({case_, oldCase}))
+    log(JSON.stringify({payload: req.body}))
+    log(JSON.stringify({oldCase}))
 
     // const doctor = await databases.getDocument(DB_ID, COLLECTION_DOCTORS, case_.doctorId);
     // const doctorDue = Math.max(doctor.due || 0 + case_.due, 0);
